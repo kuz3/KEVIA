df592e8ef53e0e577dbc9e087ddd662850328284
Kuz3 <kuz3kav@gmail.com>
2019-08-04 15:56:59 -0600

    fixed call to drinks_start()

--- mainmenu.py
+++ mainmenu.py
@@ -1,5 +1,5 @@
-#import menu.drinksmenu
-#import menu.morality
-#import menu.questions
+from menu.drinksmenu import * 
+import menu.morality
+import menu.questions
 #import menu.life tentative title
 #import menu.learn tentative title
