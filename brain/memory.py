2de34af1a013ceb1b84dff4e9f0c8ad230ec7b48
Kuz3 <kuz3kav@gmail.com>
2019-08-09 14:43:04 -0600

    Update memory.py

--- brain/memory.py
+++ brain/memory.py
@@ -1,4 +1,22 @@
 
+import consciousness.moodcurrent as c
+import random as r
+###############################################################################
+#==================================MEMORY FUNCTIONS=============================
+###############################################################################
+
+def hi(next): #print from bank and store next in bank.
+  global hibank
+  hibank.append(next)
+  print r.choice(hibank)
+def massmemory(you_said): #one memory captures all #again do I even need to put the var in here
+    global massmemory
+    massmembank.append(you_said)
+def ask():
+  you_said = raw_input(c.mood_current).lower()
+  massmemory(you_said)
+  return you_said #what am I returning...
+
 def memory(next):
     global membank
     membank.append(next)
