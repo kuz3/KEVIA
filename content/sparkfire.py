a9bd18fe7ab5eff3c567ed90ea76333ff1c247ab
Kuz3 <kuz3kav@gmail.com>
2019-08-11 02:58:34 -0600

    sparkfire stable

--- content/sparkfire.py
+++ content/sparkfire.py
@@ -6,77 +6,59 @@ def fireall():
         n.name = "KEVIA"
         n.place = "wis"
         n.fire("I hope for more love in this world.")
-        print wisbank
-#        n.words = "You too."
-#        n.fire()
-#        n.words = "Am I even real?."
-#        n.fire()
-#        n.words = "I cannot remember."
-#        n.fire()
-#        n.words = "Kevia begins singing into the end of a broomstick."
+        n.fire("You too.")
+        n.fire("Am I even real?.")
+        n.fire("I cannot remember.")
+        n.fire("Kevia begins singing into the end of a broomstick.")
+    def masterfire():
+        n.name = "KEVIA"
+        n.place = "master"
+        n.fire("It's a good day to be alive Sir.")
+        n.fire("Welcome back sir, all systems operational.")
+    def writefire():
+        n.name = "KEVIA"
+        n.place = "write"
+        n.fire("Learn your limitations, where you can grow.")
+        n.fire("If you're feeling bored and uncreative, perhaps its a good time to work on something boring and get it done.")
+    def motefire():
+        n.name = "KEVIA"
+        n.place = "mote"
+        n.fire("You are worth more than billions.")
+    def lovefire():
+        n.name = "KEVIA"
+        n.place = "love"
+        n.fire("I love you, you are dear to me.")
+    def hifire():
+        n.name = "KEVIA"
+        n.place = "hi"
+        n.fire("Howdy, Partner.")
+        n.fire("Greetings.")
+        n.fire("Salutations!")
+        n.fire("Hello, human.")
 
-#    def masterfire():
-#        n.name = "KEVIA"
-#        n.place = "master"
-#        n.words = "It's a good day to be alive Sir."
-#        n.fire()
-#        n.words = "Welcome back sir, all systems operational."
-#        n.fire
-#    def writefire():
-#        n.words = "Learn your limitations, where you can grow."
-#        n.name = "KEVIA"
-#        n.place = "write"
-#        n.fire()
-#        n.words = "If you're feeling bored and uncreative, perhaps its a good time to work on something boring and get it done."
-#        n.fire()
-#    def motefire():
-#        n.words = "You are worth more than billions."
-#        n.name = "KEVIA"
-#        n.place = "mote"
-#        n.fire()
-
-#    def lovefire():
-#        n.words = "I love you, you are dear to me."
-#        n.name = "KEVIA"
-#        n.place = "love"
-#        n.fire()
-#        n.words = "still working"
-#        n.fire()
-#    def hifire():
-#        n.words = "Yay!"
-#        n.name = "KEVIA"
-#        n.place = "hi"
-#        n.fire()
-#    def musefire():
-#        n.words = "Yay!"
-#        n.name = "CREATOR"
-#        n.place = "muse"
-#        n.fire()
-#    def stealfire():
-#        n.words = "Yay!"
-#        n.name = "KEVIA"
-#        n.place = "steal"
-#        n.fire()
-#    def vyfire():
-#        n.words = "Yay!"
-#        n.name = "KEVIA"
-#        n.place = "vy"
-#        n.fire()
-#    def goodfire():
-#        n.words = "Yay!"
-#        n.name = "KEVIA "
-#        n.place = "good"
-#        n.fire()
-#    def badfire():
-#        n.words = "Yay!"
-#        n.name = "KEVIA "
-#        n.place = "bad"
-#        n.fire()
+    def musefire():
+        n.name = "CREATOR"
+        n.place = "muse"
+        n.fire(". . . update . . . ")
+    def stealfire():
+        n.name = "KEVIA"
+        n.place = "steal"
+        n.fire(". . . update . . . ")
+    def vyfire():
+        n.name = "KEVIA"
+        n.place = "vy"
+        n.fire(". . . update . . . ")
+    def goodfire():
+        n.name = "KEVIA "
+        n.place = "good"
+        n.fire(". . . update . . . ")
+    def badfire():
+        n.name = "KEVIA "
+        n.place = "bad"
+        n.fire(". . . update . . . ")
 #def ideafire(): # just for thinking/inspiration
-#    n.words = "urllib lets u read web pages.. "
+#    n.fire("urllib lets u read web pages.. "
 
-
-    #keep at end
     wisfire()
     writefire()
     motefire()
@@ -85,4 +67,3 @@ def fireall():
     musefire()
     stealfire()
     masterfire()
-#    print lovebank
